c1c62a719f0a97a664e5b03aea910b49dfa63cb0
Javier Lopez <jalopez13@gmail.com>
2024-09-26 01:47:54 -0700

    Update2 (#15)
    * update: made updted
    
    * update: made updted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import { Character } from '@/components/character';
 const IndexPage = async () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-6">
-      <p className="text-2xl font-semibold">
-        Generate Starwars Character - new change
-      </p>
+      <p className="text-2xl font-semibold">Generate Starwars Character!</p>
       <Character />
     </div>
   );
