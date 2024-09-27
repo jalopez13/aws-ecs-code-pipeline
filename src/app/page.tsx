75d7b72d03c66577e5ca147aa39704735f53db36
Javier Lopez <jalopez13@gmail.com>
2024-09-26 18:03:23 -0700

    feat: added new layout for app (#16)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import { Character } from '@/components/character';
 const IndexPage = async () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-6">
-      <p className="text-2xl font-semibold">Generate Starwars Character!</p>
+      <h1 className="text-4xl font-semibold">Starwars Character</h1>
+      <p className="text-lg text-gray-600">
+        Generate a random Starwars character, ... why? Why not!
+      </p>
       <Character />
     </div>
   );
